235017aa5691a7f0b729bafc74a28366b35dc67b
hyqiu <qiu.henri@gmail.com>
2019-01-25 23:24:01 +0100

    Fixed behaviourtoken path

--- contracts/BehaviourToken.sol
+++ contracts/BehaviourToken.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20Detailed.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract BehaviourToken is ERC20, ERC20Detailed {
     
